ec2ba813655e25332ce1c315d4a6f67f57c7742a
clofsj <clofsj@ya.ru>
2023-06-24 10:29:15 -0400

    change route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+Route::get('categories', [CategoryController::class, 'index']);
+Route::get('category/{id}', [CategoryController::class, 'show']);
+Route::delete('category/{id}', [CategoryController::class, 'destroy']);
+Route::post('category', [CategoryController::class, 'store']);
+
+Route::get('products', [ProductController::class, 'index']);
+Route::post('product', [ProductController::class, 'store']);
+Route::put('product/{id}', [ProductController::class, 'update']);
+Route::delete('product/{id}', [ProductController::class, 'destroy']);
+Route::get('product/{id}', [ProductController::class, 'show']);
+
